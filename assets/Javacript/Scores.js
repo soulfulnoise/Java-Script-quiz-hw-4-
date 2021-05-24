3b5aaf2c855710474b1b5313278a57d5239e24cb
soulfulnoise <leespaulding25@gmail.com>
2021-05-23 20:48:38 -0400

    added event listenrs for return but and returned local storage

--- assets/Javacript/Scores.js
+++ assets/Javacript/Scores.js
@@ -12,5 +12,24 @@ clearScore.addEventListener("click" , function (){
     }
 });
 
+//Fetching local storage
+
+var allScores = localStorage.getItem("allScores");
+allScores =JSON.parse(allScores);
+
+if(allScores !==null) {
+    for (var i=0; i < allScores.length;i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initals + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+    }
+}
+
+// return listener
+
+returnButton.addEventListener("click", function () {
+    window.location.replace("..index.html");
+});
+
 
 
